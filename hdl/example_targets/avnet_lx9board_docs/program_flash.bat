67ee7847d6e5740b3ba4ce509986a66e2006e93a
Colin O'Flynn <coflynn@newae.com>
2013-01-23 23:22:14 -0400

    Make LX9 use actual ADC data

--- hdl/example_targets/avnet_lx9board_docs/program_flash.bat
+++ hdl/example_targets/avnet_lx9board_docs/program_flash.bat
@@ -7,6 +7,6 @@ ECHO
 rem
 rem This batch file will perform a sector erase of the specified length and
 rem then write the specified file to the flash starting at address 0..
-sfutil -d obp -cr -m N25Q128 -e -fi -w ../avnet_lx9board_ise/avnet_lx9_openadc.mcs -t
+sfutil -d obp -cr -m N25Q128 -e -fi -w ../avnet_lx9board_ise/openadc_lx9.mcs -t
 ECHO
 ECHO Press any key to exit...
