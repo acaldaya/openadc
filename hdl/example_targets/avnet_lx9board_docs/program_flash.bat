5ed2d01e77d180acc92ba2c5b5fa94df617c5e1f
Colin O'Flynn <coflynn@newae.com>
2013-01-23 20:42:19 -0400

    Update LX9 Project

---
+++ hdl/example_targets/avnet_lx9board_docs/program_flash.bat
@@ -0,0 +1,12 @@
+cls
+echo off
+cls
+ECHO *** Spartan-6 LX9 MicroBoard Flash Loader with Erase ***
+ECHO ------------------------------------------------------------------------
+ECHO
+rem
+rem This batch file will perform a sector erase of the specified length and
+rem then write the specified file to the flash starting at address 0..
+sfutil -d obp -cr -m N25Q128 -e -fi -w ../avnet_lx9board_ise/avnet_lx9_openadc.mcs -t
+ECHO
+ECHO Press any key to exit...
